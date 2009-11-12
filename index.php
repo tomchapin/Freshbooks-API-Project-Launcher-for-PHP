3b3215227956f5d27c109a2d968056185a96fd8b
Tom Chapin <tchapin@gmail.com>
2009-11-11 22:06:35 -0700

    Updated index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 // Initialize required libraries and includes
 include_once("includes/general.php");
 
-
-
 ?>
+The Freshbooks API Project Launcher (for PHP) is currently under heavy development. Be sure to keep an eye on the Git project page!<br />
+<a href="http://github.com/tomchapin/Freshbooks-API-Project-Launcher-for-PHP">http://github.com/tomchapin/Freshbooks-API-Project-Launcher-for-PHP</a>
